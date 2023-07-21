329ab220776a4170443ff858392ecb188ca4561b
ksharsha72 <Harsha070@outlook.com>
2023-07-21 11:14:06 +0530

    added mode2

--- S7/models.py
+++ S7/models.py
@@ -61,3 +61,37 @@ class Model2(nn.Module):
         x = self.pool2(F.relu(self.block4(x)))
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=1)
+
+
+class Model3(nn.Module):
+    def __init__(self):
+        super(Model3, self).__init__()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(1, 16, 3), nn.BatchNorm2d(16), nn.Dropout(0.1)
+        )
+
+        self.block2 = nn.Sequential(
+            nn.Conv2d(16, 8, 3), nn.BatchNorm2d(8), nn.Dropout(0.1)
+        )
+        self.transition_block1 = nn.Conv2d(8, 10, 1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.block3 = nn.Sequential(
+            nn.Conv2d(10, 12, 3), nn.BatchNorm2d(12), nn.Dropout(0.1)
+        )
+
+        self.block4 = nn.Sequential(
+            nn.Conv2d(12, 6, 3), nn.BatchNorm2d(6), nn.Dropout(0.1)
+        )
+
+        self.block5 = nn.Conv2d(6, 10, 1)
+        self.pool2 = nn.AvgPool2d(8)
+
+    def forward(self, x):
+        x = F.relu(self.block1(x))
+        x = F.relu(self.block2(x))
+        x = self.pool1(self.transition_block1(x))
+        x = F.relu(self.block3(x))
+        x = self.pool2(F.relu(self.block5((F.relu(self.block4(x))))))
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=1)
