d19f95fc0aba42e4e81c510e9e7ed8e3a40823d6
ksharsha72 <Harsha070@outlook.com>
2023-07-21 15:10:37 +0530

    Added the model3

--- S7/models.py
+++ S7/models.py
@@ -9,19 +9,20 @@ class Model1(nn.Module):
     def __init__(self):
         super(Model1, self).__init__()
         self.block1 = nn.Sequential(
-            nn.Conv2d(1, 12, 3), nn.BatchNorm2d(12), nn.Dropout(0.1)
+            nn.Conv2d(1, 14, 3), nn.BatchNorm2d(14), nn.Dropout(0.1)
         )
 
         self.block2 = nn.Sequential(
-            nn.Conv2d(12, 8, 3), nn.BatchNorm2d(8), nn.Dropout(0.1)
+            nn.Conv2d(14, 10, 3), nn.BatchNorm2d(10), nn.Dropout(0.1)
         )
-        self.transition_block1 = nn.Conv2d(8, 14, 1)
+        self.transition_block1 = nn.Conv2d(10, 8, 1)
         self.pool1 = nn.MaxPool2d(2, 2)
 
         self.block3 = nn.Sequential(
-            nn.Conv2d(14, 12, 3), nn.BatchNorm2d(12), nn.Dropout(0.1)
+            nn.Conv2d(8, 12, 3), nn.BatchNorm2d(12), nn.Dropout(0.1)
         )
-        self.block4 = nn.Conv2d(12, 10, 1)
+        self.block4 = nn.Conv2d(12, 10, 3)
+        self.block5 = nn.Conv2d(10, 10, 1)
         self.pool2 = nn.AvgPool2d(10)
 
     def forward(self, x):
@@ -29,7 +30,7 @@ class Model1(nn.Module):
         x = F.relu(self.block2(x))
         x = self.pool1(self.transition_block1(x))
         x = F.relu(self.block3(x))
-        x = self.pool2(F.relu(self.block4(x)))
+        x = F.relu(self.block5(F.relu(self.block4(x))))
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=1)
 
