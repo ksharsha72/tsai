8ef9721ace9c874f21c6a54919478b5cfddc35bc
ksharsha72 <Harsha070@outlook.com>
2023-07-22 09:55:49 +0530

    added the code for s7

--- S7/models.py
+++ S7/models.py
@@ -135,3 +135,41 @@ class Model4(nn.Module):
         x = self.pool2(self.transition_block2(x))
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=1)
+
+
+class Model5(nn.Module):
+    def __init__(self):
+        super(Model5, self).__init__()
+        self.block1 = nn.Sequential(
+            nn.Conv2d(1, 16, 3), nn.ReLU(), nn.BatchNorm2d(16), nn.Dropout(0.25)
+        )
+
+        self.block2 = nn.Sequential(
+            nn.Conv2d(16, 12, 3), nn.ReLU(), nn.BatchNorm2d(12), nn.Dropout(0.25)
+        )
+
+        self.transition_block1 = nn.Conv2d(12, 24, 1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.block3 = nn.Sequential(
+            nn.Conv2d(24, 12, 3), nn.ReLU(), nn.BatchNorm2d(12), nn.Dropout(0.25)
+        )
+
+        self.block4 = nn.Sequential(
+            nn.Conv2d(12, 14, 3), nn.ReLU(), nn.BatchNorm2d(14), nn.Dropout(0.25)
+        )
+
+        self.block5 = nn.Sequential(nn.Conv2d(14, 11, 3), nn.ReLU())
+        self.transition_block2 = nn.Conv2d(11, 10, 1)
+        self.pool2 = nn.AvgPool2d(6)
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.pool1(self.transition_block1(x))
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.pool2(self.transition_block2(x))
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=1)
