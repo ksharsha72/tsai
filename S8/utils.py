f27ab5db113749b32ab3ccf379d4ef60841fca91
ksharsha72 <Harsha070@outlook.com>
2023-07-24 20:56:01 +0530

    visualize kernels

--- S8/utils.py
+++ S8/utils.py
@@ -93,8 +93,8 @@ def test(model, device, test_loader, epoch):
         if test_acc[-1] >= sorted(test_acc)[-1]:
             global incorrect_preds, incorrect_data, original_target
             torch.save(model.state_dict(), "./best_model.pth")
-            incorrect_preds.append(pred[torch.where(~(pred == target)).numpy()])
-            original_target.append(target[np.where(~(pred == target)).numpy()])
+            incorrect_preds.append(pred[torch.where(~(pred == target))[0].numpy()])
+            original_target.append(target[np.where(~(pred == target))[0].numpy()])
             incorrect_data.append(data[incorrect_preds[-1]])
         print("The Test Accuracy is", test_acc[epoch - 1])
 
