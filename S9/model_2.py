49020966c5cb398cdc62a6f9d01f520583c5ec6e
ksharsha72 <Harsha070@outlook.com>
2023-07-28 13:50:52 +0530

    network with strides

--- S9/model_2.py
+++ S9/model_2.py
@@ -8,64 +8,64 @@ class Model(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super(Model, self).__init__(*args, **kwargs)
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, 3),
-            nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Dropout(0.1),
-        )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, 3),
+            nn.Conv2d(3, 32, 3),
             nn.ReLU(),
             nn.BatchNorm2d(32),
             nn.Dropout(0.1),
         )
-
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 48, 3, stride=2, padding=2),
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 48, 3),
             nn.ReLU(),
             nn.BatchNorm2d(48),
             nn.Dropout(0.1),
         )
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(48, 56, 3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(56),
-            nn.Dropout(0.1),
-        )
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(56, 64, 3, padding=1),
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(48, 64, 3, stride=2),
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(0.1),
         )
-        self.layer6 = nn.Sequential(
-            nn.Conv2d(64, 48, 3, stride=2, padding=2),
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.1),
+        )
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(32, 48, 3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(48),
             nn.Dropout(0.1),
         )
-        self.dw_sep1 = nn.Sequential(
-            nn.Conv2d(48, 96, 3, groups=48, padding=1),
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(48, 64, 3, stride=2, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(96),
+            nn.BatchNorm2d(64),
             nn.Dropout(0.1),
-            nn.Conv2d(96, 32, 1),
+        )
+        self.dw_sep1 = nn.Sequential(
+            nn.Conv2d(64, 64, 3, groups=64, padding=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+            nn.Conv2d(64, 48, 1),
         )
         self.layer8 = nn.Sequential(
-            nn.Conv2d(32, 64, 3, padding=1),
+            nn.Conv2d(48, 64, 3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(0.1),
         )
 
         self.layer9 = nn.Sequential(
-            nn.Conv2d(64, 32, 3, stride=2, padding=4),
+            nn.Conv2d(64, 32, 3, stride=2, padding=2),
             nn.ReLU(),
             nn.BatchNorm2d(32),
             nn.Dropout(0.1),
         )
         self.layer10 = nn.Sequential(
-            nn.Conv2d(32, 48, 3, padding=1),
+            nn.Conv2d(32, 48, 3, padding=2),
             nn.ReLU(),
             nn.BatchNorm2d(48),
             nn.Dropout(0.1),
@@ -77,8 +77,7 @@ class Model(nn.Module):
             nn.Dropout(0.1),
         )
         self.layer12 = nn.Sequential(nn.Conv2d(56, 10, 3, stride=2))
-
-        self.out = nn.AvgPool2d(3)
+        self.out = nn.AvgPool2d(2)
 
     def forward(self, x):
         x = self.layer1(x)
@@ -93,6 +92,5 @@ class Model(nn.Module):
         x = self.layer10(x)
         x = self.layer11(x)
         x = self.layer12(x)
-        x = self.out(x)
         x = x.view(-1, 10)
         return F.log_softmax(x)
