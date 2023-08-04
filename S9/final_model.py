f19fedbcfbf5f0407e1a8ec001775aa18858da95
ksharsha72 <Harsha070@outlook.com>
2023-08-04 12:55:45 +0530

    try with final model

--- S9/final_model.py
+++ S9/final_model.py
@@ -70,7 +70,7 @@ class Net(nn.Module):
         )
 
         self.layer9 = nn.Sequential(
-            nn.Conv2d(56, 32, 3, stride=2, padding=2, padding_mode="reflect"),
+            nn.Conv2d(56, 32, 3, stride=2, padding=4, padding_mode="reflect"),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Dropout(0.1),
@@ -84,10 +84,10 @@ class Net(nn.Module):
         )
 
         self.layer11 = nn.Sequential(
-            nn.Conv2d(48, 24, 3, padding=1, padding_mode="reflect"),
-            nn.BatchNorm2d(24),
-            nn.ReLU(),
-            nn.Dropout(0.1),
+            nn.Conv2d(48, 24, 3),
+            # nn.BatchNorm2d(24),
+            # nn.ReLU(),
+            # nn.Dropout(0.1),
         )
 
         self.layer12 = nn.Sequential(
