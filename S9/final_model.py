2af555a2eb9b98594c37ec61d7c3116a85191de1
ksharsha72 <Harsha070@outlook.com>
2023-08-04 12:12:43 +0530

    try with final model

---
+++ S9/final_model.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super(Net, self).__init__(*args, **kwargs)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1, padding_mode="reflect"),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, dilation=2, padding=2, padding_mode="reflect"),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+        self.layer2_1 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, groups=32, padding=1, padding_mode="reflect"),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 72, 3, stride=2),
+            nn.BatchNorm2d(72),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(72, 216, 3, groups=72, padding_mode="reflect", padding=1),
+            nn.BatchNorm2d(216),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(216, 56, 1),
+        )
+
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(56, 72, 3, padding=1, padding_mode="reflect"),
+            nn.BatchNorm2d(72),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(72, 24, 3, stride=2, padding=4, padding_mode="reflect"),
+            nn.BatchNorm2d(24),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(24, 36, 3, padding=1, padding_mode="reflect"),
+            nn.BatchNorm2d(36),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(36, 56, 3, padding=1, padding_mode="reflect"),
+            nn.BatchNorm2d(56),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(56, 32, 3, stride=2, padding=2, padding_mode="reflect"),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(32, 48, 3, padding=1, padding_mode="reflect"),
+            nn.BatchNorm2d(48),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(48, 24, 3, padding=1, padding_mode="reflect"),
+            nn.BatchNorm2d(24),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(24, 10, 3, stride=2, padding=2, padding_mode="reflect"),
+            nn.BatchNorm2d(10),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.gap = nn.AvgPool2d(5)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x) + self.layer2_1(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+        x = self.layer8(x)
+        x = self.layer9(x)
+        x = self.layer10(x)
+        x = self.layer11(x)
+        x = self.layer12(x)
+        x = self.gap(x)
+        return F.log_softmax(x)
