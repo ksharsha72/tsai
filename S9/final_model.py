124dc38f54ba4163e5196b4ab885c364ef9b72a4
ksharsha72 <Harsha070@outlook.com>
2023-08-04 14:52:34 +0530

    try with final model

--- S9/final_model.py
+++ S9/final_model.py
@@ -20,11 +20,14 @@ class Net(nn.Module):
         )
 
         self.layer3 = nn.Sequential(
-            nn.Conv2d(64, 56, 3, stride=2, padding=2),
+            nn.Conv2d(64, 56, 3, stride=2, padding=5, padding_mode="reflect"),
+            nn.BatchNorm2d(56),
+            nn.ReLU(),
+            nn.Dropout(0.1),
         )
 
         self.layer4 = nn.Sequential(
-            nn.Conv2d(56, 168, 3, groups=56, padding=1),
+            nn.Conv2d(56, 168, 3, groups=56, padding_mode="reflect", padding=1),
             nn.BatchNorm2d(168),
             nn.ReLU(),
             nn.Dropout(0.1),
@@ -32,44 +35,68 @@ class Net(nn.Module):
         )
 
         self.layer5 = nn.Sequential(
-            nn.Conv2d(56, 64, 3, padding=1),
+            nn.Conv2d(56, 64, 3, padding=1, padding_mode="reflect"),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Dropout(0.1),
         )
 
         self.layer6 = nn.Sequential(
-            nn.Conv2d(64, 24, 3, stride=2, padding=2),
+            nn.Conv2d(64, 24, 3, stride=2, padding=4, padding_mode="reflect"),
+            nn.BatchNorm2d(24),
+            nn.ReLU(),
+            nn.Dropout(0.1),
         )
 
         self.layer7 = nn.Sequential(
-            nn.Conv2d(24, 36, 3, padding=1),
+            nn.Conv2d(24, 36, 3, padding=1, padding_mode="reflect"),
             nn.BatchNorm2d(36),
             nn.ReLU(),
             nn.Dropout(0.1),
         )
 
         self.layer7_2 = nn.Sequential(
-            nn.Conv2d(24, 36, 3, dilation=2, padding=2),
+            nn.Conv2d(24, 36, 3, dilation=2, padding=2, padding_mode="reflect"),
             nn.BatchNorm2d(36),
             nn.ReLU(),
             nn.Dropout(0.1),
         )
 
         self.layer8 = nn.Sequential(
-            nn.Conv2d(36, 56, 3, padding=1),
+            nn.Conv2d(36, 56, 3, padding=1, padding_mode="reflect"),
             nn.BatchNorm2d(56),
             nn.ReLU(),
             nn.Dropout(0.1),
         )
 
         self.layer9 = nn.Sequential(
-            nn.Conv2d(56, 32, 3),
+            nn.Conv2d(56, 32, 3, stride=2, padding=4, padding_mode="reflect"),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(0.1),
         )
 
-        self.gap = nn.AvgPool2d(4)
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(32, 48, 3, padding=1, padding_mode="reflect"),
+            nn.BatchNorm2d(48),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
 
-        self.layer10 = nn.Conv2d(32, 10, 1)
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(48, 24, 3),
+            nn.BatchNorm2d(24),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+        )
+
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(24, 16, 3, stride=2),
+        )
+
+        self.gap = nn.AvgPool2d(3)
+
+        self.fc = nn.Linear(16, 10)
 
     def forward(self, x):
         x = self.layer1(x)
@@ -81,7 +108,10 @@ class Net(nn.Module):
         x = self.layer7(x) + self.layer7_2(x)
         x = self.layer8(x)
         x = self.layer9(x)
-        x = self.gap(x)
         x = self.layer10(x)
-        x = x.view(-1, 10)
+        x = self.layer11(x)
+        x = self.layer12(x)
+        x = self.gap(x)
+        x = x.view(-1, 16)
+        x = self.fc(x)
         return F.log_softmax(x)
---
+++ S9/model copy.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super(Model, self).__init__(*args, **kwargs)
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.1),
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 48, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(48),
+            nn.Dropout(0.1),
+        )
+        self.dil1 = nn.Sequential(
+            nn.Conv2d(48, 64, 3, padding=1, dilation=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(48, 64, 3),
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 56, 3),
+            nn.ReLU(),
+            nn.BatchNorm2d(56),
+            nn.Dropout(0.1),
+        )
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(56, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.1),
+        )
+        self.dil2 = nn.Sequential(
+            nn.Conv2d(64, 32, 3, dilation=2, padding=2),
+        )
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(64, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.1),
+        )
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(32, 48, 3),
+            nn.ReLU(),
+            nn.BatchNorm2d(48),
+            nn.Dropout(0.1),
+        )
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(48, 32, 3, stride=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.1),
+        )
+        self.dw_sep2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, groups=32, padding=1),
+            nn.Conv2d(64, 48, 1),
+        )
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(48, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.1),
+        )
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(32, 24, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(24),
+            nn.Dropout(0.1),
+        )
+        self.layer12 = nn.Sequential(nn.Conv2d(24, 10, 3, stride=2), nn.ReLU())
+
+        self.out = nn.AvgPool2d(5)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.dil1(x) + self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x) + self.dil2(x)
+        x = self.layer7(x)
+        x = self.layer8(x)
+        x = self.dw_sep2(x)
+        x = self.layer10(x)
+        x = self.layer11(x)
+        x = self.layer12(x)
+        x = self.out(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x)
