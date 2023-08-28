16c9598ea9521395485bc217f5bc60e9eadda39f
ksharsha72 <Harsha070@outlook.com>
2023-08-28 12:34:13 +0530

    changes to the model

---
+++ S9/base_model.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class BaseModel(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super(BaseModel, self).__init__(*args, **kwargs)
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3), nn.ReLU(), nn.BatchNorm2d(32), nn.Dropout(0.1)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Dropout(0.1)
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 48, 3), nn.ReLU(), nn.BatchNorm2d(48), nn.Dropout(0.1)
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(48, 56, 3), nn.ReLU(), nn.BatchNorm2d(56), nn.Dropout(0.1)
+        )
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(56, 64, 3), nn.ReLU(), nn.BatchNorm2d(64), nn.Dropout(0.1)
+        )
+
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(64, 32, 3), nn.ReLU(), nn.BatchNorm2d(32), nn.Dropout(0.1)
+        )
+
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(32, 24, 3), nn.ReLU(), nn.BatchNorm2d(24), nn.Dropout(0.1)
+        )
+
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(24, 48, 3), nn.ReLU(), nn.BatchNorm2d(48), nn.Dropout(0.1)
+        )
+
+        self.conv9 = nn.Sequential(
+            nn.Conv2d(48, 56, 3), nn.ReLU(), nn.BatchNorm2d(56), nn.Dropout(0.1)
+        )
+
+        self.conv10 = nn.Sequential(
+            nn.Conv2d(56, 32, 3), nn.ReLU(), nn.BatchNorm2d(32), nn.Dropout(0.1)
+        )
+
+        self.conv11 = nn.Sequential(
+            nn.Conv2d(32, 36, 3), nn.ReLU(), nn.BatchNorm2d(36), nn.Dropout(0.1)
+        )
+
+        self.conv12 = nn.Sequential(
+            nn.Conv2d(36, 20, 3), nn.ReLU(), nn.BatchNorm2d(20), nn.Dropout(0.1)
+        )
+
+        self.out = nn.Conv2d(20, 10, 3)
+        self.gap = nn.AvgPool2d(6)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
+        x = self.conv9(x)
+        x = self.conv10(x)
+        x = self.conv11(x)
+        x = self.conv12(x)
+        x = self.out(x)
+        x = self.gap(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x)
