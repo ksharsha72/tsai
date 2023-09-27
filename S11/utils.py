aaa6b00266acad0c5176c764f890a60de73d58f0
ksharsha72 <Harsha070@outlook.com>
2023-09-27 13:45:09 +0530

    Added the Grad-Cam Model

--- S11/utils.py
+++ S11/utils.py
@@ -68,8 +68,9 @@ def wrong_predictions(model):
             fig.show()
             fig.tight_layout()
             plt.tight_layout()
+            tens = torch.from_numpy(np_trans)
             rgb_img = test_transforms(image=np_trans)["image"]
-            show_grad_cam_image(model, np_trans, rgb_img)
+            show_grad_cam_image(model, tens, rgb_img)
 
 
 def show_kernels(param, kernel_size):
